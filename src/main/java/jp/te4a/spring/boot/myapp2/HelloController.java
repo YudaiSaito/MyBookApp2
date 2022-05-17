a38fb2930b383054a84d02c3c061804730fd3c58
YudaiSaito <tejc190711@gmail.com>
2022-05-17 12:41:37 +0900

    Fix: ymlのコンテキストパスの書き方

--- src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController 
 {
-	@RequestMapping("/te/taro")
+	@RequestMapping("/taro")
 	public String taro() {
 		return "Taro desu!";
 	}
